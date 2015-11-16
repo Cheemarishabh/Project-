b05106c4dcb447d2efd2f62099a2ade38766bec1
Cheemarishabh <rishabhcheema5@gmail.com>
2015-11-16 17:39:08 +0530

    Update cnt.php

--- cnt.php
+++ cnt.php
@@ -1,13 +1,13 @@
 <?php
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
-$db_host = 'localhost';
-$db_user = 'root';
-$db_pwd = ''; 
+$host = 'localhost';
+$user = 'root';
+$pwd = ''; 
 
 $database = 'miniproject';
 $table ='baby';
-$conn = @mysql_connect($db_host, $db_user, $db_pwd) or
+$conn = @mysql_connect($host, $user, $pwd) or
 die("cant select database ");
 
 if (!mysql_select_db($database))
